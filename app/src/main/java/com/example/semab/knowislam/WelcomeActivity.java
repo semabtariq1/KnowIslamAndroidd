9fd1e1abbf11b9d2192eabb2aed0aa587587a16e
semab <semab.tariq1@gmail.com>
2018-03-08 16:08:24 +0500

    first uploading semab 8 4

---
+++ app/src/main/java/com/example/semab/knowislam/WelcomeActivity.java
@@ -0,0 +1,21 @@
+package com.example.semab.knowislam;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    // to go on next activity from welcome screne on next button press
+    public void welcomeNext(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
