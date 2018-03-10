c992225a8cb56a0de7bb4548b3eda19ba456d932
zubair1111 <zubair.akram090@gmail.com>
2018-03-10 13:53:02 +0500

    zubair

--- app/src/main/java/com/example/semab/knowislam/WelcomeActivity.java
+++ app/src/main/java/com/example/semab/knowislam/WelcomeActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
-//aaaaaaaaaaaaaa
+//kkaaaaaaaaaaaaaa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
