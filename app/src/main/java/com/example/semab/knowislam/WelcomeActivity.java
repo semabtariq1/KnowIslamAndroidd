ebf9565a1f1db32e411e9c839e121cf5b3095662
semab <semab.tariq1@gmail.com>
2018-03-10 19:21:45 +0500

    updated by semab

--- app/src/main/java/com/example/semab/knowislam/WelcomeActivity.java
+++ app/src/main/java/com/example/semab/knowislam/WelcomeActivity.java
@@ -4,19 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class WelcomeActivity extends AppCompatActivity {
-//kkaaaaaaaaaaaaaakksal
+
+    LinearLayout l1,l2;
+    Button btnsub;
+    Animation uptodown,downtoup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        // set animation
+
+        btnsub = (Button)findViewById(R.id.buttonsub);
+        l1 = (LinearLayout) findViewById(R.id.l1);
+        l2 = (LinearLayout) findViewById(R.id.l2);
+        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        l1.setAnimation(uptodown);
+        l2.setAnimation(downtoup);
     }
 
     // to go on next activity from welcome screne on next button press
     public void welcomeNext(View v){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-        //update
+
+
     }
 }
--- app/src/main/java/com/example/semab/knowislam/abc.java
+++
@@ -1,9 +0,0 @@
-package com.example.semab.knowislam;
-
-/**
- * Created by zubai on 3/10/2018.
- */
-
-public class abc {
-//kkkk
-}
