14687a72f0c3ef281e585c67c60d1433bd0fc2ad
semab <semab.tariq1@gmail.com>
2018-03-17 12:03:45 +0500

    Merge branch 'master' of C:\Users\semab\AndroidStudioProjects\KnowIslam with conflicts.

--- app/src/main/java/com/example/semab/knowislam/MainActivity.java
+++ app/src/main/java/com/example/semab/knowislam/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.semab.knowislam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String serverPath = "192.168.1.4";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void getNextActtivity(View v){
+
+
+        Intent intent = new Intent(this, MainList.class);
+        startActivity(intent);
+
     }
 }
